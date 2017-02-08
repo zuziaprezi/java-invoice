0c010b021dd7f94edeea2e3f2beefa4169be6820
Wojciech Frącz <fraczwojciech@gmail.com>
2017-02-08 22:34:03 +0100

    Clean the implementation

--- src/main/java/pl/edu/agh/mwo/java2/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/java2/Invoice.java
@@ -4,39 +4,32 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 public class Invoice {
-	
-	
-	private Invoice() {
-		
-	}
-	
 	public void addProduct(Product product) {
 		// TODO: implement
 	}
-	
+
 	public void addProduct(Product product, Integer quantity) {
 		// TODO: implement
 	}
-	
+
 	public String getNumber() {
 		// TODO: implement
 		return null;
 	}
-	
+
 	public BigDecimal getSubtotal() {
 		return null;
 	}
-	
+
 	public BigDecimal getTax() {
 		return null;
 	}
-	
+
 	public BigDecimal getTotal() {
 		return null;
 	}
-	
+
 	public static Invoice create() {
 		return new Invoice();
 	}
-	
 }
--- src/test/java/pl/edu/agh/mwo/java2/ProductTest.java
+++ src/test/java/pl/edu/agh/mwo/java2/ProductTest.java
@@ -7,121 +7,66 @@ import java.math.BigDecimal;
 import org.junit.Test;
 
 public class ProductTest {
-
-	private static final String _1_00 = "-1.00";
-	private static final String _108_0 = "108.0";
-	private static final String _0_08 = "0.08";
-	private static final String _0_23 = "0.23";
-	private static final String _100_0 = "100.0";
 	private static final String PRODUCT_1 = "Product 1";
 
 	@Test
 	public void testProductNameIsCorrect() {
-		// given
-		Product product = Product.create(PRODUCT_1, new BigDecimal(_100_0));
-
-		// when
-		// then
+		Product product = Product.create(PRODUCT_1, new BigDecimal("100.0"));
 		assertEquals(PRODUCT_1, product.getName());
 
 	}
 
-	
 	@Test
 	public void testProductPriceAndTaxWithDefaultTax() {
-		// given
-		Product product = Product.create(PRODUCT_1, new BigDecimal(_100_0));
-
-		// when
-		// then
-		assertBigDecimalsAreEquals(_100_0, product.getPrice());
-		assertBigDecimalsAreEquals(_0_23, product.getTaxPercent());
-
+		Product product = Product.create(PRODUCT_1, new BigDecimal("100.0"));
+		assertBigDecimalsAreEquals("100.0", product.getPrice());
+		assertBigDecimalsAreEquals("0.23", product.getTaxPercent());
 	}
 
 	@Test
 	public void testProductPriceAndTaxWithCustomTax() {
-		// given
-		Product product = Product.create(PRODUCT_1, new BigDecimal(_100_0), new BigDecimal(_0_08));
-
-		// when
-		// then	
-		assertBigDecimalsAreEquals(_100_0, product.getPrice());
-		assertBigDecimalsAreEquals(_0_08, product.getTaxPercent());
+		Product product = Product.create(PRODUCT_1, new BigDecimal("100.0"), new BigDecimal("0.08"));
+		assertBigDecimalsAreEquals("100.0", product.getPrice());
+		assertBigDecimalsAreEquals("0.08", product.getTaxPercent());
 	}
 
 	@Test
 	public void testPriceWithTax() {
-		// given
-		Product product = Product.create(PRODUCT_1, new BigDecimal(_100_0), new BigDecimal(_0_08));
-
-		// when
-		// then
-		assertBigDecimalsAreEquals(_108_0, product.getPriceWithTax());
+		Product product = Product.create(PRODUCT_1, new BigDecimal("100.0"), new BigDecimal("0.08"));
+		assertBigDecimalsAreEquals("108.0", product.getPriceWithTax());
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testProductWithNullName() {
-		// given
-		Product.create(null, new BigDecimal(_100_0), new BigDecimal(_0_08));
-
-		// when
-		// then
-		// IllegalArgumentException is thrown
+		Product.create(null, new BigDecimal("100.0"), new BigDecimal("0.08"));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testProductWithEmptyName() {
-		// given
-		Product.create("", new BigDecimal(_100_0), new BigDecimal(_0_08));
-
-		// when
-		// then
-		// IllegalArgumentException is thrown
+		Product.create("", new BigDecimal("100.0"), new BigDecimal("0.08"));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testProductWithNullPrice() {
-		// given
-		Product.create(PRODUCT_1, null, new BigDecimal(_0_08));
-
-		// when
-		// then
-		// IllegalArgumentException is thrown
+		Product.create(PRODUCT_1, null, new BigDecimal("0.08"));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testProductWithNegativePrice() {
-		// given
-		Product.create(PRODUCT_1, new BigDecimal(_1_00), new BigDecimal(_0_08));
-
-		// when
-		// then
-		// IllegalArgumentException is thrown
+		Product.create(PRODUCT_1, new BigDecimal("-1.00"), new BigDecimal("0.08"));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testProductWithNullTax() {
-		// given
-		Product.create(PRODUCT_1, new BigDecimal(_100_0), null);
-
-		// when
-		// then
-		// IllegalArgumentException is thrown
+		Product.create(PRODUCT_1, new BigDecimal("100.0"), null);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testProductWithNegativeTax() {
-		// given
-		Product.create(PRODUCT_1, new BigDecimal(_100_0), new BigDecimal(_1_00));
-
-		// when
-		// then
-		// IllegalArgumentException is thrown
+		Product.create(PRODUCT_1, new BigDecimal("100.0"), new BigDecimal("-1.00"));
 	}
 
 	private void assertBigDecimalsAreEquals(String expected, BigDecimal actual) {
 		assertEquals(0, new BigDecimal(expected).compareTo(actual));
 	}
-
 }
