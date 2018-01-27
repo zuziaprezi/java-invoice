a0a5b450b4aca5908d32edb3682ef043f6e622b1
Wojciech Frącz <fraczwojciech@gmail.com>
2018-01-27 22:19:03 +0100

    Be clear that Invoice contains products

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,10 +1,13 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
+	private Collection<Product> products;
+
 	public void addProduct(Product product) {
 		// TODO: implement
 	}
